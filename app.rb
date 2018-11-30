200144b75fe70977ceee3d907b4356f55d57d43b
Paige Williams <paw145@humboldt.edu>
2018-11-30 10:03:17 -0800

    Gather user input from form and add input into list of words and their definitions.

--- app.rb
+++ app.rb
@@ -6,5 +6,16 @@ require('./lib/definer')
 
 
 get('/') do
+  @word_list = Word.word_list()
+  erb(:input)
+end
+
+post('/output')do
+  word = params[:word]
+  definition = params[:definition]
+  @input_word = Word.new(word, definition)
+  @input_word.add_word(word, definition)
+  @word_list = Word.word_list()
+  binding.pry
   erb(:input)
 end
