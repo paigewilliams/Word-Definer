6e1f3a24dcc9ac2e773e04e6a1113f6b7d8363d5
Paige Williams <paw145@humboldt.edu>
2018-11-30 10:39:46 -0800

    Add words to the list in UI.

--- app.rb
+++ app.rb
@@ -4,18 +4,25 @@ require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/definer')
 
-
 get('/') do
   @word_list = Word.word_list()
+
   erb(:input)
 end
 
-post('/output')do
+get('/words')do
+  @word_list = Word.word_list()
+  @@display = "display:none"
+  erb(:output)
+end
+
+post('/words')do
+  @@display = "display:block"
   word = params[:word]
   definition = params[:definition]
   @input_word = Word.new(word, definition)
   @input_word.add_word(word, definition)
   @word_list = Word.word_list()
-  binding.pry
-  erb(:input)
+
+  erb(:output)
 end
